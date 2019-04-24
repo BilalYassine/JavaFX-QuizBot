dd56ae3e07118716d8645b7171b2877413478285
Evan Hansen <ehansen8@wisc.edu>
2019-04-24 08:42:10 -0500

    Added Saving Capability to FileSystem

--- QuizProjectFX/application/FileSystem.java
+++ QuizProjectFX/application/FileSystem.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -10,15 +12,49 @@ public class FileSystem
 
     public static void loadFile(Stage stage)
     {
+        fileChooser.setTitle("Select Quiz Questions");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JSON", "*.json*"));
+
         File file = fileChooser.showOpenDialog(stage);
         if (file != null)
         {
             // openFile(file); calls appropriate file import methods/class
         }
     }
-    
+
+    public static void saveJSON(Stage stage)
+    {
+        fileChooser.setTitle("Save JSON");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JSON", "*.json*"));
+        File file = fileChooser.showSaveDialog(stage);
+        if (!file.getName().contains("."))
+        {
+            file = new File(file.getAbsolutePath() + ".json");
+        }
+
+        if (file != null)
+        {
+            try
+            {
+                FileWriter writer = new FileWriter(file);
+                // PrintWriter print = new PrintWriter(writer);
+                writer.write("TEST JSON");      // Replace with JSON file Generator
+                writer.close();
+            } catch (IOException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
     public static void loadImage(Stage stage)
     {
+        fileChooser.setTitle("Select Question Image");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Files", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg*"),
+                new FileChooser.ExtensionFilter("PNG", "*.PNG*"));
         File file = fileChooser.showOpenDialog(stage);
         if (file != null)
         {
