0df5dd4966d4785e3e92e3139d8daa2fc57a8926
Evan Hansen <ehansen8@wisc.edu>
2019-04-23 20:33:09 -0500

    Switched to FileSystem

--- QuizProjectFX/application/GUIHomeController.java
+++ QuizProjectFX/application/GUIHomeController.java
@@ -1,10 +1,12 @@
 package application;
 
 
-    
+
 import java.io.File;
 import java.util.List;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -18,35 +20,41 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 
-public class GUIHomeController extends Scene {
-    public GUIHomeController(Parent root,Stage Primary, List<Scene> sceneList) {
-      super(root,800,600);
-      BorderPane parent = (BorderPane)root;
-      
-      parent.setPadding(new Insets(10, 10, 10, 10));
-      HBox buttons = new HBox(10);
-      buttons.setAlignment(Pos.CENTER);
-      Button quizButton = new Button("Take Quiz");
-      quizButton.setOnAction(e ->Primary.setScene(sceneList.get(1)));
-      quizButton.setPrefSize(100,100);
-      
-      Button loadQuiz = new Button("Load Quiz");
-      loadQuiz.setPrefSize(100,100);
-      loadQuiz.setOnAction(
-              new EventHandler<ActionEvent>() {
-                  @Override
-                  public void handle(final ActionEvent e) {
-                      FileSystem.loadFile(Primary);
-                  }
-              });
-      
-      Button addQuestionButton = new Button("Add Question");
-      addQuestionButton.setOnAction(e ->Primary.setScene(sceneList.get(2)));
-      addQuestionButton.setPrefSize(100,100);
-      
-      buttons.getChildren().addAll(quizButton,loadQuiz,addQuestionButton);
-      parent.setBottom(buttons);
-  }
-    
+public class GUIHomeController extends Scene
+{
+    public GUIHomeController(Parent root, Stage Primary, List<Scene> sceneList)
+    {
+        super(root, 800, 600);
+        BorderPane parent = (BorderPane) root;
+        this.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        HBox titleBox = new HBox(10);
+        Label title = new Label("Quiz Generator!");
+        title.setId("title-text");
+        titleBox.getChildren().addAll(title);
+        titleBox.setAlignment(Pos.CENTER);
+        parent.setCenter(titleBox);
+
+        parent.setPadding(new Insets(10, 10, 10, 10));
+        HBox buttons = new HBox(10);
+        buttons.setAlignment(Pos.CENTER);
+        Button quizButton = new Button("Take Quiz");
+        quizButton.setOnAction(e -> Primary.setScene(sceneList.get(1)));
+        quizButton.setPrefSize(100, 100);
+
+        Button loadQuiz = new Button("Load Quiz");
+        loadQuiz.setPrefSize(100, 100);
+        loadQuiz.setOnAction(EventHandler -> {
+            FileSystem.loadFile(Primary);
+        });
+
+        Button addQuestionButton = new Button("Add Question");
+        addQuestionButton.setOnAction(e -> Primary.setScene(sceneList.get(2)));
+        addQuestionButton.setPrefSize(100, 100);
+
+        buttons.getChildren().addAll(quizButton, loadQuiz, addQuestionButton);
+        parent.setBottom(buttons);
+    }
+
 }
 
