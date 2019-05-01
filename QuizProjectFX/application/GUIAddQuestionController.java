9e1f46d56a62f961e56722000da8b5175ff59e93
OwenThronson <othronson@wisc.edu>
2019-04-30 20:59:06 -0500

    Add Question

--- QuizProjectFX/application/GUIAddQuestionController.java
+++ QuizProjectFX/application/GUIAddQuestionController.java
@@ -24,7 +24,7 @@ import javafx.scene.text.Font;
 
 
 public class GUIAddQuestionController extends Scene {
-    public GUIAddQuestionController(Parent root,Stage Primary, List<Scene> sceneList) {
+    public GUIAddQuestionController(Parent root,Stage Primary, List<Scene> sceneList, QuestionTable table) {
       super(root,800,600);
       BorderPane parent = (BorderPane)root;
       this.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -157,7 +157,7 @@ public class GUIAddQuestionController extends Scene {
         correctText.clear();
         metaText.clear();
         questionText.clear();
-        
+        table.AddQuestion(q);
       });
       returnButton.setOnAction(e ->Primary.setScene(sceneList.get(0)));
       
